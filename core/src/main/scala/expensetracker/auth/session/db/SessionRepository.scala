7f0c04af1d422157270beb872fe8caa5349066b1
kirill5k <immotional@aol.com>
2021-06-12 13:47:13 +0100

    SessionRepository

---
+++ core/src/main/scala/expensetracker/auth/session/db/SessionRepository.scala
@@ -0,0 +1,39 @@
+package expensetracker.auth.session.db
+
+import cats.effect.Async
+import cats.implicits._
+import com.mongodb.client.model.Filters
+import io.circe.generic.auto._
+import expensetracker.auth.account.AccountId
+import expensetracker.auth.session.{Session, SessionId}
+import mongo4cats.database.{MongoCollectionF, MongoDatabaseF}
+import mongo4cats.circe._
+import org.bson.types.ObjectId
+
+import scala.concurrent.duration.FiniteDuration
+
+trait SessionRepository[F[_]] {
+  def create(aid: AccountId, duration: FiniteDuration): F[SessionId]
+  def find(sid: SessionId): F[Option[Session]]
+}
+
+final private class LiveSessionRepository[F[_]: Async](
+    private val collection: MongoCollectionF[SessionEntity]
+) extends SessionRepository[F] {
+
+  override def create(aid: AccountId, duration: FiniteDuration): F[SessionId] = {
+    val createSession = SessionEntity.create(aid, duration)
+    collection.insertOne[F](createSession).as(SessionId(createSession.id.toHexString))
+  }
+
+  override def find(sid: SessionId): F[Option[Session]] =
+    collection
+      .find(Filters.eq("id", new ObjectId(sid.value)))
+      .first[F]
+      .map(res => Option(res).map(_.toDomain))
+}
+
+object SessionRepository {
+  def make[F[_]: Async](db: MongoDatabaseF[F]): F[SessionRepository[F]] =
+    db.getCollectionWithCirceCodecs[SessionEntity]("sessions").map(coll => new LiveSessionRepository[F](coll))
+}
