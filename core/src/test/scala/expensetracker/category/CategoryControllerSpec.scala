ea89e1f1f3921486a33bd638c262f41750caafa2
kirill5k <immotional@aol.com>
2021-06-15 19:51:58 +0100

    category controller spec

---
+++ core/src/test/scala/expensetracker/category/CategoryControllerSpec.scala
@@ -0,0 +1,22 @@
+package expensetracker.category
+
+import expensetracker.ControllerSpec
+
+class CategoryControllerSpec extends ControllerSpec {
+
+  "A CategoryController" when {
+
+    "GET /categories" should {
+
+      "return user's categories" in {
+
+      }
+    }
+
+    "DELETE /categories/:id" should {
+      "delete category by id" in {
+
+      }
+    }
+  }
+}
