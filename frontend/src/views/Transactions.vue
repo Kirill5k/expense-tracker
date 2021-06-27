45f72137c0b01b63b08c2d1bd6bb5a289345ea05
kirill5k <immotional@aol.com>
2021-06-27 08:53:28 +0100

    transactions and categories pages

---
+++ frontend/src/views/Transactions.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-container
+    class="transactions"
+    fluid
+  >
+    <v-row
+      justify="center"
+    >
+      <v-col
+        cols="12"
+        xs="9"
+        sm="6"
+        md="4"
+        lg="3"
+      >
+        <v-card
+          :loading="loading"
+          class="mx-auto"
+        >
+          <v-card-title>
+            Transactions
+          </v-card-title>
+
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+
+export default {
+  name: 'Transactions',
+  data: () => ({
+    loading: false
+  })
+}
+</script>
+
+<style lang="scss">
+.transactions {
+
+}
+</style>
