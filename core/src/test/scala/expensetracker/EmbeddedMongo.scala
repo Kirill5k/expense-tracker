c44ac8d4a8a557ee918ca0ec423d3e3a2c926b00
kirill5k <immotional@aol.com>
2021-07-07 20:22:39 +0100

    remove unused imports

--- core/src/test/scala/expensetracker/EmbeddedMongo.scala
+++ core/src/test/scala/expensetracker/EmbeddedMongo.scala
@@ -1,6 +1,6 @@
 package expensetracker
 
-import de.flapdoodle.embed.mongo.{MongodExecutable, MongodProcess, MongodStarter}
+import de.flapdoodle.embed.mongo.MongodStarter
 import de.flapdoodle.embed.mongo.config.{MongodConfig, Net}
 import de.flapdoodle.embed.mongo.distribution.Version
 import de.flapdoodle.embed.process.runtime.Network
