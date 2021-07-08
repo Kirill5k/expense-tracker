bceaf7b7bc9b51909f48c90a54c17d6f4aabf4a8
kirill5k <immotional@aol.com>
2021-07-08 09:35:22 +0100

    basic acc info

---
+++ frontend/src/components/account/AccountDetails.vue
@@ -0,0 +1,52 @@
+<template>
+  <v-row>
+    <v-col
+      xs="4"
+      sm="4"
+      md="3"
+      cols="4"
+      class="pr-0"
+    >
+      <v-icon
+        size="100"
+        dense
+        color="black"
+        class="mx-auto"
+      >
+        mdi-account
+      </v-icon>
+    </v-col>
+    <v-col
+      xs="8"
+      sm="8"
+      md="8"
+      cols="8"
+      class="pl-0"
+    >
+      <v-list>
+        <v-list-item>
+          <v-list-item-content class="py-0">
+            <v-list-item-title class="mt-9 text-h6">
+              {{ account.firstName }}
+              {{ account.lastName }}
+            </v-list-item-title>
+            <v-list-item-subtitle>{{account.email}}</v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+
+export default {
+  name: 'AccountDetails',
+  props: {
+    account: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
--- frontend/src/views/Account.vue
+++ frontend/src/views/Account.vue
@@ -7,16 +7,33 @@
     <v-card-title>
       Account
     </v-card-title>
+
+    <v-card-text>
+      <account-details
+        :account="account"
+      />
+      <v-divider/>
+    </v-card-text>
   </v-card>
 </template>
 
 <script>
+import AccountDetails from '@/components/account/AccountDetails'
+
 export default {
   name: 'Account',
+  components: {
+    AccountDetails
+  },
   data: () => ({
     loading: false,
     editable: false
-  })
+  }),
+  computed: {
+    account () {
+      return this.$store.state.account
+    }
+  }
 }
 </script>
 
