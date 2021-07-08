27d06de4d533f6b3a9378e36b1c6fe16ab0ca46d
kirill5k <immotional@aol.com>
2021-07-08 20:45:14 +0100

    filter by currency

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -48,6 +48,9 @@ export default new Vuex.Store({
       acc[el.id] = el
       return acc
     }, {}),
+    filteredTransactions: state => state.transactions
+      .filter(t => t.amount.currency.code === state.account.settings.currency.code)
+      .filter(t => state.account.settings.hideFutureTransactions ? new Date(t.date) <= new Date() : true),
     displayedTransactions: state => ({
       current: withinDates(state.transactions, state.displayDate),
       previous: withinDates(state.transactions, state.displayDate.previous || {})
