264aa888bc3356e9ed304ed5cf923544ef2bf2e7
kirill5k <immotional@aol.com>
2021-07-18 12:48:07 +0100

    restyle icon picker

--- frontend/src/components/categories/NewCategoryDialog.vue
+++ frontend/src/components/categories/NewCategoryDialog.vue
@@ -65,13 +65,14 @@
                 item-height="40"
               >
                 <template v-slot:default="{ item }">
-                  <v-row class="text-center" no-gutters>
+                  <v-row v-if="item.length" class="text-center" no-gutters>
                     <v-col
                       v-for="icon in item"
                       :key="icon.value"
                       cols="2"
                     >
                       <v-btn
+                        :color="newCategory.icon === icon.value ? newCategory.color : ''"
                         :dark="newCategory.icon === icon.value"
                         :light="newCategory.icon !== icon.value"
                         elevation="2"
@@ -85,6 +86,12 @@
                         </v-icon>
                       </v-btn>
                     </v-col>
+                  </v-row>
+                  <v-row v-else no-gutters>
+                    <v-card-text class="py-1">
+                      <p class="text-subtitle-2 mb-0">{{ item.header }}</p>
+                      <v-divider class="my-1"></v-divider>
+                    </v-card-text>
                   </v-row>
                 </template>
               </v-virtual-scroll>
@@ -135,94 +142,94 @@ const DEFAULT_CATEGORY = {
 
 const ICONS = [
   { header: 'Bills', divider: true },
-  { value: 'mdi-antenna', text: 'antenna', disable: false },
-  { value: 'mdi-flash', text: 'flash', disable: false },
-  { value: 'mdi-fire', text: 'fire', disable: false },
-  { value: 'mdi-home', text: 'home', disable: false },
-  { value: 'mdi-lightbulb', text: 'lightbulb', disable: false },
-  { value: 'mdi-umbrella', text: 'umbrella', disable: false },
-  { value: 'mdi-water-pump', text: 'water-pump', disable: false },
-  { value: 'mdi-web', text: 'web', disable: false },
-  { value: 'mdi-wifi', text: 'wifi', disable: false },
+  { value: 'mdi-antenna' },
+  { value: 'mdi-flash' },
+  { value: 'mdi-fire' },
+  { value: 'mdi-home' },
+  { value: 'mdi-lightbulb' },
+  { value: 'mdi-umbrella' },
+  { value: 'mdi-water-pump' },
+  { value: 'mdi-web' },
+  { value: 'mdi-wifi' },
   { header: 'Food, drink', divider: true },
-  { value: 'mdi-beer', text: 'beer', disable: false },
-  { value: 'mdi-bottle-soda', text: 'bottle-soda', disable: false },
-  { value: 'mdi-bowl-mix', text: 'bowl-mix', disable: false },
-  { value: 'mdi-coffee', text: 'coffee', disable: false },
-  { value: 'mdi-cookie', text: 'cookie', disable: false },
-  { value: 'mdi-cup', text: 'cup', disable: false },
-  { value: 'mdi-cupcake', text: 'cupcake', disable: false },
-  { value: 'mdi-fish', text: 'fish', disable: false },
-  { value: 'mdi-food-fork-drink', text: 'food-fork-drink', disable: false },
-  { value: 'mdi-glass-cocktail', text: 'glass-cocktail', disable: false },
-  { value: 'mdi-grill', text: 'grill', disable: false },
-  { value: 'mdi-hamburger', text: 'hamburger', disable: false },
+  { value: 'mdi-beer' },
+  { value: 'mdi-bottle-soda' },
+  { value: 'mdi-bowl-mix' },
+  { value: 'mdi-coffee' },
+  { value: 'mdi-cookie' },
+  { value: 'mdi-cup' },
+  { value: 'mdi-cupcake' },
+  { value: 'mdi-fish' },
+  { value: 'mdi-food-fork-drink' },
+  { value: 'mdi-glass-cocktail' },
+  { value: 'mdi-grill' },
+  { value: 'mdi-hamburger' },
   { header: 'Entertainment', divider: true },
-  { value: 'mdi-camera', text: 'camera', disable: false },
-  { value: 'mdi-cellphone', text: 'cellphone', disable: false },
-  { value: 'mdi-guitar-acoustic', text: 'guitar-acoustic', disable: false },
-  { value: 'mdi-headphones', text: 'headphones', disable: false },
-  { value: 'mdi-microphone', text: 'microphone', disable: false },
-  { value: 'mdi-monitor', text: 'monitor', disable: false },
-  { value: 'mdi-music', text: 'music', disable: false },
-  { value: 'mdi-piano', text: 'piano', disable: false },
-  { value: 'mdi-tablet', text: 'tablet', disable: false },
-  { value: 'mdi-television', text: 'television', disable: false },
-  { value: 'mdi-watch', text: 'watch', disable: false },
+  { value: 'mdi-camera' },
+  { value: 'mdi-cellphone' },
+  { value: 'mdi-guitar-acoustic' },
+  { value: 'mdi-headphones' },
+  { value: 'mdi-microphone' },
+  { value: 'mdi-monitor' },
+  { value: 'mdi-music' },
+  { value: 'mdi-piano' },
+  { value: 'mdi-tablet' },
+  { value: 'mdi-television' },
+  { value: 'mdi-watch' },
   { header: 'Holiday', divider: true },
-  { value: 'mdi-bed-double', text: 'bed-double', disable: false },
-  { value: 'mdi-cake', text: 'cake', disable: false },
-  { value: 'mdi-coffin', text: 'coffin', disable: false },
-  { value: 'mdi-firework', text: 'firework', disable: false },
-  { value: 'mdi-food-turkey', text: 'food-turkey', disable: false },
-  { value: 'mdi-gift', text: 'gift', disable: false },
-  { value: 'mdi-halloween', text: 'halloween', disable: false },
-  { value: 'mdi-party-popper', text: 'party-popper', disable: false },
-  { value: 'mdi-pine-tree', text: 'pine-tree', disable: false },
-  { value: 'mdi-snowflake', text: 'snowflake', disable: false },
+  { value: 'mdi-bed-double' },
+  { value: 'mdi-cake' },
+  { value: 'mdi-coffin' },
+  { value: 'mdi-firework' },
+  { value: 'mdi-food-turkey' },
+  { value: 'mdi-gift' },
+  { value: 'mdi-halloween' },
+  { value: 'mdi-party-popper' },
+  { value: 'mdi-pine-tree' },
+  { value: 'mdi-snowflake' },
   { header: 'Money', divider: true },
-  { value: 'mdi-account-cash', text: 'account-cash', disable: false },
-  { value: 'mdi-bank', text: 'bank', disable: false },
-  { value: 'mdi-bitcoin', text: 'bitcoin', disable: false },
-  { value: 'mdi-cash', text: 'cash', disable: false },
-  { value: 'mdi-credit-card', text: 'credit-card', disable: false },
-  { value: 'mdi-currency-eur', text: 'currency-eur', disable: false },
-  { value: 'mdi-currency-gbp', text: 'currency-gbp', disable: false },
-  { value: 'mdi-currency-usd', text: 'currency-usd', disable: false },
-  { value: 'mdi-finance', text: 'finance', disable: false },
-  { value: 'mdi-piggy-bank', text: 'piggy-bank', disable: false },
-  { value: 'mdi-safe', text: 'safe', disable: false },
-  { value: 'mdi-wallet', text: 'wallet', disable: false },
+  { value: 'mdi-account-cash' },
+  { value: 'mdi-bank' },
+  { value: 'mdi-bitcoin' },
+  { value: 'mdi-cash' },
+  { value: 'mdi-credit-card' },
+  { value: 'mdi-currency-eur' },
+  { value: 'mdi-currency-gbp' },
+  { value: 'mdi-currency-usd' },
+  { value: 'mdi-finance' },
+  { value: 'mdi-piggy-bank' },
+  { value: 'mdi-safe' },
+  { value: 'mdi-wallet' },
   { header: 'Shopping', divider: true },
-  { value: 'mdi-basket', text: 'basket', disable: false },
-  { value: 'mdi-cart', text: 'cart', disable: false },
-  { value: 'mdi-cash-register', text: 'cash-register', disable: false },
-  { value: 'mdi-shopping', text: 'shopping', disable: false },
-  { value: 'mdi-store', text: 'store', disable: false },
-  { value: 'mdi-wallet-giftcard', text: 'wallet-giftcard', disable: false },
+  { value: 'mdi-basket' },
+  { value: 'mdi-cart' },
+  { value: 'mdi-cash-register' },
+  { value: 'mdi-shopping' },
+  { value: 'mdi-store' },
+  { value: 'mdi-wallet-giftcard' },
   { header: 'Sport', divider: true },
-  { value: 'mdi-basketball', text: 'basketball', disable: false },
-  { value: 'mdi-bike', text: 'bike', disable: false },
-  { value: 'mdi-bowling', text: 'bowling', disable: false },
-  { value: 'mdi-dumbbell', text: 'dumbbell', disable: false },
-  { value: 'mdi-run', text: 'run', disable: false },
-  { value: 'mdi-soccer', text: 'soccer', disable: false },
-  { value: 'mdi-tennis', text: 'tennis', disable: false },
-  { value: 'mdi-trophy', text: 'trophy', disable: false },
-  { value: 'mdi-yoga', text: 'yoga', disable: false },
+  { value: 'mdi-basketball' },
+  { value: 'mdi-bike' },
+  { value: 'mdi-bowling' },
+  { value: 'mdi-dumbbell' },
+  { value: 'mdi-run' },
+  { value: 'mdi-soccer' },
+  { value: 'mdi-tennis' },
+  { value: 'mdi-trophy' },
+  { value: 'mdi-yoga' },
   { header: 'Travel', divider: true },
-  { value: 'mdi-airplane', text: 'airplane', disable: false },
-  { value: 'mdi-ambulance', text: 'ambulance', disable: false },
-  { value: 'mdi-bag-carry-on', text: 'bag-carry-on', disable: false },
-  { value: 'mdi-bag-suitcase', text: 'bag-suitcase', disable: false },
-  { value: 'mdi-bus', text: 'bus', disable: false },
-  { value: 'mdi-car', text: 'car', disable: false },
-  { value: 'mdi-rocket', text: 'rocket', disable: false },
-  { value: 'mdi-train', text: 'train', disable: false },
-  { value: 'mdi-tram', text: 'tram', disable: false },
-  { value: 'mdi-walk', text: 'walk', disable: false },
-  { value: 'mdi-van-passenger', text: 'van-passenger', disable: false },
-  { value: 'mdi-van-utility', text: 'van-utility', disable: false }
+  { value: 'mdi-airplane' },
+  { value: 'mdi-ambulance' },
+  { value: 'mdi-bag-carry-on' },
+  { value: 'mdi-bag-suitcase' },
+  { value: 'mdi-bus' },
+  { value: 'mdi-car' },
+  { value: 'mdi-rocket' },
+  { value: 'mdi-train' },
+  { value: 'mdi-tram' },
+  { value: 'mdi-walk' },
+  { value: 'mdi-van-passenger' },
+  { value: 'mdi-van-utility' }
 ]
 
 export default {
@@ -245,21 +252,25 @@ export default {
   computed: {
     groupedIcons () {
       return this.icons
-        .filter(i => i.divider !== true)
-        .reduce((res, item, index) => {
-          const chunkIndex = Math.floor(index / 6)
-          if (!res[chunkIndex]) {
-            res[chunkIndex] = []
+        .reduce((res, item) => {
+          if (item.divider === true) {
+            res.push(item)
+          } else {
+            const pos = res.length - 1
+            if (!res[pos]) {
+              res[pos] = []
+            }
+            if (res[pos].length < 6) {
+              res[pos].push(item)
+            } else {
+              res.push([item])
+            }
           }
-          res[chunkIndex].push(item)
           return res
         }, [])
     }
   },
   methods: {
-    formatIconName (icon) {
-      return icon.charAt(0).toUpperCase() + icon.slice(1).replaceAll('-', ' ')
-    },
     reset () {
       this.newCategory = { ...DEFAULT_CATEGORY }
       this.valid = true
