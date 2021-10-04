815c01e42629835eae6ad6673dd9468cee4c0595
kirill5k <immotional@aol.com>
2021-10-04 16:54:58 +0100

    Refactor Action to be enum

--- core/src/main/scala/expensetracker/common/actions/Action.scala
+++ core/src/main/scala/expensetracker/common/actions/Action.scala
@@ -2,8 +2,6 @@ package expensetracker.common.actions
 
 import expensetracker.auth.user.UserId
 
-sealed trait Action
-
-object Action {
-  final case class SetupNewUser(id: UserId) extends Action
+enum Action {
+  case SetupNewUser(id: UserId) extends Action
 }
--- core/src/main/scala/expensetracker/health/HealthController.scala
+++ core/src/main/scala/expensetracker/health/HealthController.scala
@@ -8,20 +8,19 @@ import org.http4s.HttpRoutes
 import org.http4s.circe.CirceEntityCodec._
 
 final class HealthController[F[_]: Monad] extends Controller[F] {
-
   val routes: HttpRoutes[F] =
-    HttpRoutes.of[F] {
-      case GET -> Root / "health" / "status" => Ok(AppStatus.UP)
-    }
+    HttpRoutes.of[F] { case GET -> Root / "health" / "status" => Ok(AppStatus.UP) }
 }
 
 object HealthController {
 
   final case class AppStatus(status: Boolean)
+
   object AppStatus {
     val UP = AppStatus(true)
   }
 
   def make[F[_]: Monad]: F[HealthController[F]] =
     Monad[F].pure(new HealthController[F])
+
 }
