ddb2318287feb34301658932b81698c7985e2e9c
kirill5k <immotional@aol.com>
2022-01-07 16:54:48 +0000

    Update dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
     val pureConfig = "0.17.1"
     val circe      = "0.14.1"
     val http4s     = "0.23.7"
-    val logback    = "1.2.8"
+    val logback    = "1.2.10"
     val log4cats   = "2.1.1"
     val squants    = "1.8.3"
     val bcrypt     = "4.3.0"
