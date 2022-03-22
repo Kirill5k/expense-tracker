6cd24f24ede51688aeafc30e6a6adadf40ce6c68
kirill5k <immotional@aol.com>
2022-03-22 09:13:48 +0000

    Simplift repository code

--- core/src/main/scala/expensetracker/common/db/Repository.scala
+++ core/src/main/scala/expensetracker/common/db/Repository.scala
@@ -1,10 +1,10 @@
 package expensetracker.common.db
 
 import cats.MonadError
-import cats.syntax.applicative.*
-import cats.syntax.applicativeError.*
+import cats.syntax.option.*
 import com.mongodb.client.result.UpdateResult
 import expensetracker.auth.user.UserId
+import expensetracker.common.types.IdType
 import mongo4cats.bson.ObjectId
 import mongo4cats.collection.operations.{Filter, Update}
 
@@ -25,16 +25,16 @@ trait Repository[F[_]] {
 
   protected val notHidden: Filter = Filter.ne(Field.Hidden, true)
 
-  private def idEqFilter(name: String, id: String): Filter = Filter.eq(name, ObjectId(id))
-  protected def idEq(id: String): Filter                   = idEqFilter(Field.Id, id)
-  protected def userIdEq(aid: Option[UserId]): Filter      = idEqFilter(Field.UId, aid.map(_.value).orNull)
-  protected def userIdEq(aid: UserId): Filter              = idEqFilter(Field.UId, aid.value)
+  private def idEqFilter(name: String, id: Option[String]): Filter = Filter.eq(name, id.map(ObjectId.apply).orNull)
+  protected def idEq(id: String): Filter                           = idEqFilter(Field.Id, id.some)
+  protected def userIdEq(aid: Option[UserId]): Filter              = idEqFilter(Field.UId, aid.map(_.value))
+  protected def userIdEq(aid: UserId): Filter                      = idEqFilter(Field.UId, aid.value.some)
 
   protected def errorIfNull[A](error: Throwable)(res: A)(using F: MonadError[F, Throwable]): F[A] =
     F.fromOption(Option(res), error)
 
   protected def errorIfNoMatches(error: Throwable)(res: UpdateResult)(using F: MonadError[F, Throwable]): F[Unit] =
-    if (res.getMatchedCount > 0) F.unit else error.raiseError[F, Unit]
+    F.raiseWhen(res.getMatchedCount > 0)(error)
 
   protected def updateHidden(hidden: Boolean): Update =
     Update.set(Field.Hidden, hidden).currentDate(Field.LastUpdatedAt)
