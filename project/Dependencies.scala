496e8accceff69e0a3d37acf3a840450b27118ae
kirill5k <immotional@aol.com>
2022-07-26 18:35:27 +0300

    Update dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -11,8 +11,8 @@ object Dependencies {
     val squants    = "1.8.3"
     val bcrypt     = "4.3.0"
     val refined    = "0.10.1"
-    val tapir      = "1.0.1"
-    val jwt        = "9.0.5"
+    val tapir      = "1.0.2"
+    val jwt        = "9.0.6"
 
     val scalaTest = "3.2.12"
     val mockito   = "3.2.10.0"
