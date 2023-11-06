f6f4954515a1cdb9b1eea3fbbb1dcc329829b72a
kirill5k <immotional@aol.com>
2023-11-06 16:26:34 +0000

    Bump dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt.*
 
 object Dependencies {
   object Versions {
-    val mongo4cats = "0.6.15"
+    val mongo4cats = "0.6.16"
     val pureConfig = "0.17.4"
     val circe      = "0.14.6"
     val http4s     = "0.23.23"
@@ -11,7 +11,7 @@ object Dependencies {
     val refined    = "0.11.0"
     val logback    = "1.4.11"
     val log4cats   = "2.6.0"
-    val tapir      = "1.8.2"
+    val tapir      = "1.8.5"
     val jwt        = "9.4.4"
 
     val scalaTest = "3.2.17"
