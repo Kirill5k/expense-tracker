db0e2d115a60c315ce0204d315758cbeeab7037b
kirill5k <immotional@aol.com>
2024-09-16 10:47:22 +0100

    Clean other page

--- modules/mobile/screens/dashboard/analytics/index.jsx
+++ modules/mobile/screens/dashboard/analytics/index.jsx
@@ -1,16 +1,10 @@
-import {VStack} from "@/components/ui/vstack";
-import { HStack } from "@/components/ui/hstack";
-import { Box } from "@/components/ui/box";
-import { Heading } from "@/components/ui/heading";
-import { Button, ButtonIcon, ButtonText } from "@/components/ui/button";
-import { ScrollView } from "@/components/ui/scroll-view";
+import {VStack} from '@/components/ui/vstack'
+import {Heading} from '@/components/ui/heading'
+import Classes from '@/constants/classes'
 
 export const Analytics = () => {
   return (
-      <VStack
-          className="p-4 pb-0 md:px-10 md:pt-6 md:pb-0 h-full w-full max-w-[1500px] self-center  mb-20 md:mb-2"
-          space="2xl"
-      >
+      <VStack className={Classes.dashboardLayout}>
         <Heading size="2xl" className="font-roboto">
           Analytics
         </Heading>
