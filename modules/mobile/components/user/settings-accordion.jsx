e9dcb0f13ea4cd19280feace1208f5f620ebfd9e
kirill5k <immotional@aol.com>
2024-09-25 14:25:25 +0100

    Settings page layout

---
+++ modules/mobile/components/user/settings-accordion.jsx
@@ -0,0 +1,72 @@
+import {HStack} from '@/components/ui/hstack'
+import {Text} from '@/components/ui/text'
+import {Divider} from '@/components/ui/divider'
+import {
+  Accordion,
+  AccordionItem,
+  AccordionHeader,
+  AccordionTrigger,
+  AccordionTitleText,
+  AccordionContent,
+  AccordionIcon,
+} from '@/components/ui/accordion'
+import {ChevronDownIcon, ChevronUpIcon} from '@/components/ui/icon'
+
+export const SettingsAccordion = ({className, children}) => {
+  return (
+      <Accordion
+          size="sm"
+          variant="unfilled"
+          type="single"
+          isCollapsible={true}
+          className={`mb-4 border border-outline-200 rounded-xl bg-background-50 ${className}`}
+      >
+        {children}
+      </Accordion>
+  )
+}
+
+export const SettingsAccordionItem = ({isLast, value, children, headerTitle, headerValue}) => {
+  return (
+      <>
+        <AccordionItem value={value}>
+          <SettingsAccordionHeader
+              title={headerTitle}
+              value={headerValue}
+          />
+          {children}
+        </AccordionItem>
+        {!isLast && <Divider className="mx-5"/>}
+      </>
+  )
+}
+
+export const SettingsAccordionHeader = ({title, value}) => {
+  return (
+      <AccordionHeader>
+        <AccordionTrigger>
+          {({isExpanded}) => (
+              <>
+                <AccordionTitleText>{title}</AccordionTitleText>
+                <HStack className="items-center">
+                  {value != null && <Text size="sm" className="text-secondary-400">{value}</Text>}
+                  {isExpanded ? (
+                      <AccordionIcon as={ChevronUpIcon} className="ml-3"/>
+                  ) : (
+                      <AccordionIcon as={ChevronDownIcon} className="ml-3"/>
+                  )}
+                </HStack>
+              </>
+          )}
+        </AccordionTrigger>
+      </AccordionHeader>
+  )
+}
+
+export const SettingsAccordionContent = ({children}) => {
+  return (
+      <AccordionContent>
+        {children}
+      </AccordionContent>
+  )
+}
--- modules/mobile/screens/dashboard/settings/index.jsx
+++ modules/mobile/screens/dashboard/settings/index.jsx
@@ -1,8 +1,12 @@
 import {useState} from 'react'
+import {Box} from '@/components/ui/box'
+import {Button, ButtonText} from '@/components/ui/button'
 import {Heading} from '@/components/ui/heading'
 import {VStack} from '@/components/ui/vstack'
 import {ScrollView} from '@/components/ui/scroll-view'
 import Profile from '@/components/user/profile'
+import {SettingsAccordion, SettingsAccordionItem} from '@/components/user/settings-accordion'
+import {AccordionContent, AccordionContentText} from '@/components/ui/accordion'
 import Classes from '@/constants/classes'
 import useStore from '@/store'
 
@@ -11,13 +15,107 @@ export const Settings = () => {
   const {mode, user} = useStore()
   return (
       <VStack className={Classes.dashboardLayout}>
-        <Heading size={headerSize} className="font-roboto pb-2">
+        <Heading size={headerSize} className="pb-2">
           Settings
         </Heading>
         <ScrollView>
-          <Profile
-              user={user}
-          />
+          <Profile user={user}/>
+          <Heading className="py-2" size="xl">
+            General
+          </Heading>
+          <SettingsAccordion>
+            <SettingsAccordionItem
+                value="1"
+                headerTitle="Currency"
+                headerValue={user.settings.currency.symbol}
+            >
+              <AccordionContent>
+                <AccordionContentText>
+                  To place an order, simply select the products you want, proceed to
+                  checkout, provide shipping and payment information, and finalize your
+                  purchase.
+                </AccordionContentText>
+              </AccordionContent>
+            </SettingsAccordionItem>
+            <SettingsAccordionItem
+                value="2"
+                headerTitle="Hide Future Transactions"
+                headerValue={user.settings.hideFutureTransactions ? 'Yes' : 'No'}
+            >
+              <AccordionContent>
+                <AccordionContentText>
+                  To place an order, simply select the products you want, proceed to
+                  checkout, provide shipping and payment information, and finalize your
+                  purchase.
+                </AccordionContentText>
+              </AccordionContent>
+            </SettingsAccordionItem>
+            <SettingsAccordionItem
+                isLast
+                value="3"
+                headerTitle="Dark Mode"
+                headerValue={user.settings.darkMode === true ? 'On' : user.settings.darkMode === false ? 'Off' : 'Auto'}
+            >
+              <AccordionContent>
+                <AccordionContentText>
+                  To place an order, simply select the products you want, proceed to
+                  checkout, provide shipping and payment information, and finalize your
+                  purchase.
+                </AccordionContentText>
+              </AccordionContent>
+            </SettingsAccordionItem>
+          </SettingsAccordion>
+
+          <Heading className="py-2" size="xl">
+            Security
+          </Heading>
+          <SettingsAccordion>
+            <SettingsAccordionItem
+                value="4"
+                headerTitle="Change Password"
+            >
+              <AccordionContent>
+                <AccordionContentText>
+                  To place an order, simply select the products you want, proceed to
+                  checkout, provide shipping and payment information, and finalize your
+                  purchase.
+                </AccordionContentText>
+              </AccordionContent>
+            </SettingsAccordionItem>
+            <SettingsAccordionItem
+                value="5"
+                headerTitle="Erase All Transactions"
+            >
+              <AccordionContent>
+                <AccordionContentText>
+                  To place an order, simply select the products you want, proceed to
+                  checkout, provide shipping and payment information, and finalize your
+                  purchase.
+                </AccordionContentText>
+              </AccordionContent>
+            </SettingsAccordionItem>
+            <SettingsAccordionItem
+                isLast
+                value="6"
+                headerTitle="Close Account"
+            >
+              <AccordionContent>
+                <AccordionContentText>
+                  To place an order, simply select the products you want, proceed to
+                  checkout, provide shipping and payment information, and finalize your
+                  purchase.
+                </AccordionContentText>
+              </AccordionContent>
+            </SettingsAccordionItem>
+          </SettingsAccordion>
+
+          <Box className="px-5">
+            <Button className="my-4 w-full" size="xs" variant="outline" action="secondary">
+              <ButtonText>
+                Sign Out
+              </ButtonText>
+            </Button>
+          </Box>
         </ScrollView>
       </VStack>
   )
