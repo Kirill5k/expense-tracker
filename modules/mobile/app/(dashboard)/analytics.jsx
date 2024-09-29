9790206cdbf93c0a1734d1a2daf39b4e80036802
kirill5k <immotional@aol.com>
2024-09-29 08:21:53 +0100

    Move dashboard screens into app folder

--- modules/mobile/app/(dashboard)/analytics.jsx
+++ modules/mobile/app/(dashboard)/analytics.jsx
@@ -1,2 +1,43 @@
-import {Analytics} from "@/screens/dashboard/analytics";
+import {useState} from 'react'
+import {Box} from '@/components/ui/box'
+import {VStack} from '@/components/ui/vstack'
+import {Divider} from '@/components/ui/divider'
+import {Heading} from '@/components/ui/heading'
+import {ScrollView} from '@/components/ui/scroll-view'
+import {ProgressBar} from '@/components/common/progress'
+import Classes from '@/constants/classes'
+import useStore from '@/store'
+
+const Analytics = () => {
+  const [isScrolling, setIsScrolling] = useState(false)
+  const [loading, setLoading] = useState(false)
+
+  const {
+    mode
+  } = useStore()
+
+  return (
+      <VStack className={Classes.dashboardLayout}>
+        <Heading size={isScrolling ? 'sm' : '2xl'} className={loading ? 'pb-0' : 'pb-2'}>
+          Analytics
+        </Heading>
+        <Box>
+          {isScrolling && <Divider/>}
+          {loading && <ProgressBar mode={mode}/>}
+        </Box>
+        <ScrollView
+            showsVerticalScrollIndicator={false}
+            onScroll={({nativeEvent}) => {
+              if (nativeEvent.contentOffset.y <= 20 && isScrolling) {
+                setIsScrolling(false)
+              } else if (nativeEvent.contentOffset.y > 20 && !isScrolling) {
+                setIsScrolling(true)
+              }
+            }}
+        >
+        </ScrollView>
+      </VStack>
+  )
+}
+
 export default Analytics
