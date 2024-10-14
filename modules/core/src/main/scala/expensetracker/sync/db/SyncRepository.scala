b958b7e18137222736092167095f1b3c024d2b46
kirill5k <immotional@aol.com>
2024-10-14 13:40:58 +0100

    Refine aggregation pipeline

--- modules/core/src/main/scala/expensetracker/sync/db/SyncRepository.scala
+++ modules/core/src/main/scala/expensetracker/sync/db/SyncRepository.scala
@@ -7,10 +7,12 @@ import expensetracker.auth.user.UserId
 import expensetracker.common.db.Repository
 import expensetracker.common.errors.AppError
 import expensetracker.sync.db.{EntityChanges, SyncEntity}
+import mongo4cats.bson.syntax.*
 import mongo4cats.collection.MongoCollection
 import mongo4cats.database.MongoDatabase
 import mongo4cats.operations.{Aggregate, Filter, Projection, Update}
 import kirill5k.common.cats.syntax.monadthrow.*
+import mongo4cats.bson.{BsonValue, Document}
 
 import java.time.Instant
 
@@ -28,25 +30,19 @@ final private class LiveSyncRepository[F[_]](
       .updateOne(idEq(uid.toObjectId), Update.currentDate(field))
       .flatMap(r => F.whenA(r.getMatchedCount == 0)(collection.insertOne(SyncEntity.from(uid))))
 
-  private def createdFacet(fieldPrefix: String, from: Option[Instant]) =
-    Aggregate.Facet(
-      "created",
-      Aggregate.matchBy(
-        from match
-          case Some(value) => Filter.gt(fieldPrefix + Field.CreatedAt, value) && Filter.isNull(fieldPrefix + Field.LastUpdatedAt)
-          case None        => Filter.empty
-      )
-    )
+  private def createdAgg(collection: String, from: Option[Instant]) =
+    Aggregate.matchBy(
+      from match
+        case Some(value) => Filter.gt(collection + Field.CreatedAt, value) && Filter.isNull(collection + Field.LastUpdatedAt)
+        case None        => Filter.empty
+    ).project(Projection.include(collection))
 
-  private def updatedFacet(fieldPrefix: String, from: Option[Instant]) =
-    Aggregate.Facet(
-      "updated",
-      Aggregate.matchBy(
-        from match
-          case Some(value) => Filter.gt(fieldPrefix + Field.LastUpdatedAt, value)
-          case None        => Filter.isNull(Field.Id)
-      )
-    )
+  private def updatedAgg(collection: String, from: Option[Instant]) =
+    Aggregate.matchBy(
+      from match
+        case Some(value) => Filter.gt(collection + "." + Field.LastUpdatedAt, value)
+        case None        => Filter.isNull(Field.Id)
+    ).project(Projection.include(collection))
 
   def pullChanges(uid: UserId, from: Option[Instant]): F[DataChanges] =
     updateTimestamp(uid, "lastPulledAt") >>
@@ -56,21 +52,25 @@ final private class LiveSyncRepository[F[_]](
             .lookup("transactions", "_id", "userId", "transactions")
             .lookup("categories", "_id", "userId", "categories")
             .lookup("users", "_id", "_id", "users")
+            .unwind("$transactions")
+            .unwind("$categories")
+            .unwind("$users")
             .facet(
-              Aggregate.Facet(
-                "transactions",
-                Aggregate.unwind("$transactions").facet(createdFacet("transactions.", from), updatedFacet("transactions.", from))
-              ),
-              Aggregate.Facet(
-                "categories",
-                Aggregate.unwind("$categories").facet(createdFacet("categories.", from), updatedFacet("categories.", from))
-              ),
-              Aggregate.Facet(
-                "users",
-                Aggregate.unwind("$users").facet(createdFacet("users.", from), updatedFacet("users.", from))
-              )
+              Aggregate.Facet("transactionsCreated", createdAgg("transactions", from)),
+              Aggregate.Facet("categoriesCreated", createdAgg("categories", from)),
+              Aggregate.Facet("usersCreated", createdAgg("users", from)),
+              Aggregate.Facet("transactionsUpdated", updatedAgg("transactions", from)),
+              Aggregate.Facet("categoriesUpdated", updatedAgg("categories", from)),
+              Aggregate.Facet("usersUpdated", updatedAgg("users", from)),
             )
-            .project(Projection.computed("time", "$lastPulledAt"))
+            .addFields("time" -> "$$NOW")
+            .project(
+              Projection
+                .include("time")
+                .computed("users", Document("created" := "$usersCreated.users", "updated" := "$usersUpdated.users"))
+                .computed("categories", Document("created" := "$categoriesCreated.categories", "updated" := "$categoriesUpdated.categories"))
+                .computed("transactions", Document("created" := "$transactionsCreated.transactions", "updated" := "$transactionsUpdated.transactions"))
+            )
         )
         .first
         .unwrapOpt(AppError.Internal("No result when pulling sync changes"))
