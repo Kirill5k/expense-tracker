6bb782a32f66a994b316ecf7bbbd0d49ec5a33d7
kirill5k <immotional@aol.com>
2024-10-15 09:57:14 +0100

    Refactor pullChanges implementation

--- modules/core/src/main/scala/expensetracker/sync/db/SyncRepository.scala
+++ modules/core/src/main/scala/expensetracker/sync/db/SyncRepository.scala
@@ -10,7 +10,7 @@ import expensetracker.sync.DataChanges
 import mongo4cats.bson.syntax.*
 import mongo4cats.collection.MongoCollection
 import mongo4cats.database.MongoDatabase
-import mongo4cats.operations.{Aggregate, Update}
+import mongo4cats.operations.{Aggregate, Projection, Update}
 import kirill5k.common.cats.syntax.monadthrow.*
 import mongo4cats.bson.{BsonValue as BV, Document}
 
@@ -33,57 +33,69 @@ final private class LiveSyncRepository[F[_]](
   private def createdComp(collection: String, item: String, from: Option[Instant]) =
     from match
       case Some(value) =>
-        Document("$filter" := Document(
-          "input" := "$" + collection,
-          "as" := item,
-          "cond" := Document("$and" := BV.array(
-            BV.document("$lt" := BV.array(BV.string("$$" + item + ".createdAt"), BV.instant(value))),
-            BV.document("$eq" := BV.array(BV.string("$$" + item + ".lastUpdatedAt"), BV.Null))
-          ))
-        ))
+        Document(
+          "$filter" := Document(
+            "input" := "$" + collection,
+            "as"    := item,
+            "cond" := Document(
+              "$and" := BV.array(
+                BV.document("$lt" := BV.array(BV.string("$$" + item + ".createdAt"), BV.instant(value))),
+                BV.document("$eq" := BV.array(BV.string("$$" + item + ".lastUpdatedAt"), BV.Null))
+              )
+            )
+          )
+        )
       case None =>
-        Document("$filter" := Document(
-          "input" := "$" + collection,
-          "as" := item,
-          "cond" := BV.document("$ne" := BV.array(BV.string("$$" + item + ".hidden"), BV.False))
-        ))
+        Document(
+          "$filter" := Document(
+            "input" := "$" + collection,
+            "as"    := item,
+            "cond"  := BV.document("$ne" := BV.array(BV.string("$$" + item + ".hidden"), BV.False))
+          )
+        )
 
   private def updatedComp(collection: String, item: String, from: Option[Instant]) =
     from match
       case Some(value) =>
-        Document("$filter" := Document(
-          "input" := "$" + collection,
-          "as" := item,
-          "cond" := BV.document("$gt" := BV.array(BV.string("$$" + item + ".lastUpdatedAt"), BV.instant(value)))
-        ))
+        Document(
+          "$filter" := Document(
+            "input" := "$" + collection,
+            "as"    := item,
+            "cond"  := BV.document("$gt" := BV.array(BV.string("$$" + item + ".lastUpdatedAt"), BV.instant(value)))
+          )
+        )
       case None =>
-        Document("$filter" := Document(
-          "input" := "$" + collection,
-          "as" := item,
-          "cond" := BV.document("$eq" := BV.array(BV.string("$$" + item + "._id"), BV.Null))
-        ))
+        Document(
+          "$filter" := Document(
+            "input" := "$" + collection,
+            "as"    := item,
+            "cond"  := BV.document("$eq" := BV.array(BV.string("$$" + item + "._id"), BV.Null))
+          )
+        )
 
   def pullChanges(uid: UserId, from: Option[Instant]): F[DataChanges] =
     updateTimestamp(uid, "lastPulledAt") >>
       collection
         .aggregate[EntityChanges](
           Aggregate
-            .lookup("transactions", "_id", "userId", "transactions")
-            .lookup("categories", "_id", "userId", "categories")
-            .lookup("users", "_id", "_id", "users")
+            .lookup("transactions", "_id", "userId", "transactionsColl")
+            .lookup("categories", "_id", "userId", "categoriesColl")
+            .lookup("users", "_id", "_id", "usersColl")
             .addFields(
-              "time" -> "$$NOW",
-              "userCreated" -> createdComp("users", "user", from),
-              "userUpdated" -> updatedComp("users", "user", from),
-              "transactionCreated" -> createdComp("transactions", "transaction", from),
-              "transactionUpdated" -> updatedComp("transactions", "transaction", from),
-              "categoryCreated" -> createdComp("categories", "category", from),
-              "categoryUpdated" -> updatedComp("categories", "category", from),
-            ).addFields(
-              "transactions" -> Document(
-                "created" := "$transactionCreated",
-                "updated" := "$transactionUpdated"
-              )
+              "time"               -> "$$NOW",
+              "userCreated"        -> createdComp("usersColl", "user", from),
+              "userUpdated"        -> updatedComp("usersColl", "user", from),
+              "transactionCreated" -> createdComp("transactionsColl", "transaction", from),
+              "transactionUpdated" -> updatedComp("transactionsColl", "transaction", from),
+              "categoryCreated"    -> createdComp("categoriesColl", "category", from),
+              "categoryUpdated"    -> updatedComp("categoriesColl", "category", from)
+            )
+            .project(
+              Projection
+                .include("time")
+                .computed("users", Document("created" := "$userCreated", "updated" := "$userUpdated"))
+                .computed("categories", Document("created" := "$categoryCreated", "updated" := "$categoryUpdated"))
+                .computed("transactions", Document("created" := "$transactionCreated", "updated" := "$transactionUpdated"))
             )
         )
         .first
