bf0a96263e4c5ea8e4f2dff8c1e71e73ee100d6d
kirill5k <immotional@aol.com>
2024-10-24 09:52:34 +0100

    Restyle date-picker

--- modules/mobile/components/common/date-select.jsx
+++ modules/mobile/components/common/date-select.jsx
@@ -1,109 +1,74 @@
-import React, {useState} from 'react'
 import DateTimePicker from 'react-native-ui-datepicker'
-import {Button, ButtonIcon, ButtonText} from "../ui/button";
-import {Box} from "../ui/box";
-import {HStack} from "../ui/hstack";
-import {MaterialIcon} from "../ui/icon";
-import {Modal, ModalBackdrop, ModalContent, ModalBody} from "../ui/modal";
+import {Fab, FabLabel} from '../ui/fab'
+import {Box} from '../ui/box'
 import colors from '@/constants/colors'
+import {mergeClasses} from '@/utils/css'
 import dayjs from 'dayjs'
-import {format} from 'date-fns'
 
 const DateSelect = ({value, onSelect, mode}) => {
-  const [show, setShow] = useState(false)
+  const textStyle = {
+    fontSize: 12,
+    lineHeight: 20
+  }
+
+  const handleDateChange = (params) => {
+    onSelect(params.date.toDate())
+  }
 
   return (
-      <>
-        <Button
+      <Box className="border border-secondary-200 rounded-lg pb-8">
+        <DateTimePicker
+            height={200}
+            displayFullDays={false}
+            mode="single"
+            date={dayjs(value)}
+            onChange={handleDateChange}
+            headerButtonsPosition="right"
+            headerButtonColor={colors[mode].tint}
+            selectedItemColor={colors[mode].tint}
+            selectedTextStyle={{
+              fontWeight: 'bold',
+              color: colors[mode].background,
+              ...textStyle
+            }}
+            calendarTextStyle={{
+              color: colors[mode].text,
+              ...textStyle
+            }}
+            headerTextStyle={{
+              fontWeight: 'bold',
+              color: colors[mode].text,
+            }}
+            weekDaysTextStyle={{
+              color: colors[mode].text,
+              fontWeight: 600,
+              ...textStyle
+            }}
+            todayTextStyle={{
+              fontWeight: 'bold'
+            }}
+            dayContainerStyle={{
+              borderRadius: 8
+            }}
+            todayContainerStyle={{
+              borderRadius: 8
+            }}
+            headerContainerStyle={{
+              height: 24
+            }}
+        />
+        <Fab
             size="sm"
-            variant="outline"
-            action="secondary"
-            className="justify-between align-center"
-            onPress={() => setShow(true)}
+            placement="bottom right"
+            className={mergeClasses(
+                'right-1 bottom-2 rounded-lg px-0 py-1.5',
+                mode === 'light' ? 'bg-blue-600' : 'bg-blue-300'
+            )}
+            onPress={() => onSelect(new Date())}
         >
-          <ButtonIcon
-              as={MaterialIcon}
-              code="calendar"
-              dcolor={colors[mode].text}
-              dsize={20}
-              className="flex-grow-0"
-          />
-          <ButtonText className="flex-grow px-2">
-            {format(value, 'dd/MM/yyyy')}
-          </ButtonText>
-          <ButtonIcon
-              className="flex-grow-0"
-              as={MaterialIcon}
-              code={show ? 'chevron-up' : 'chevron-down'}
-              dcolor={value ? colors[mode].tabIconSelected : colors[mode].text}
-          />
-        </Button>
-        <Modal
-            isOpen={show}
-            onClose={() => setShow(false)}
-            size="md"
-        >
-          <ModalBackdrop/>
-          <ModalContent className="px-1 py-0 my-0">
-            <ModalBody>
-              <DateTimePicker
-                  mode="single"
-                  date={dayjs(value)}
-                  onChange={(params) => {
-                    onSelect(params.date.toDate())
-                    setTimeout(() => setShow(false), 200)
-                  }}
-                  headerButtonColor={colors[mode].tint}
-                  selectedItemColor={colors[mode].tint}
-                  selectedTextStyle={{
-                    fontWeight: 'bold',
-                    color: colors[mode].background,
-                  }}
-                  calendarTextStyle={{
-                    color: colors[mode].text,
-                  }}
-                  headerTextStyle={{
-                    fontWeight: 'bold',
-                    color: colors[mode].text,
-                  }}
-                  weekDaysTextStyle={{
-                    color: colors[mode].text,
-                  }}
-                  todayTextStyle={{
-                    fontWeight: 'bold'
-                  }}
-              />
-              <HStack className="mx-3 justify-end" space="sm">
-                <Button
-                    size="xs"
-                    variant="outline"
-                    action="secondary"
-                    onPress={() => {
-                      setShow(false)
-                    }}
-                >
-                  <ButtonText>
-                    Cancel
-                  </ButtonText>
-                </Button>
-                <Button
-                    size="xs"
-                    action="secondary"
-                    className={`${mode === 'light' ? 'bg-blue-600' : 'bg-blue-300'}`}
-                    onPress={() => {
-                      onSelect(new Date())
-                      setShow(false)
-                    }}
-                >
-                  <ButtonText>
-                    Today
-                  </ButtonText>
-                </Button>
-              </HStack>
-            </ModalBody>
-          </ModalContent>
-        </Modal>
-      </>
+          <FabLabel className="text-xs">Today</FabLabel>
+        </Fab>
+      </Box>
   )
 }
 
