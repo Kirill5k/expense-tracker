79ce1552449fcebd1909035ceb78a75a9d07340d
kirill5k <immotional@aol.com>
2024-10-29 19:37:40 +0000

    Reduce list item font weight

--- modules/mobile/constants/classes.js
+++ modules/mobile/constants/classes.js
@@ -2,7 +2,7 @@ export default {
   dashboardLayout: 'px-4 pt-2 pb-0 md:px-10 md:pt-6 md:pb-0 h-full w-full max-w-[1500px] self-center bg-background-0',
   scrollList: 'max-w-[600px] w-full',
   listItemLayout: 'items-center p-3',
-  listItemMainText: 'font-bold text-primary-800 line-clamp-1 p-0 m-0 text-lg',
+  listItemMainText: 'font-semibold text-primary-900 line-clamp-1 p-0 m-0 text-lg',
   listItemTag: 'rounded-lg border border-typography-800 text-typography-800 text-sm font-medium px-1',
   dark: {
     inputFieldBorder: 'border-secondary-400',
