21e071e7ff4e9095d24677d7848ae71e0c5c4984
kirill5k <immotional@aol.com>
2024-11-26 13:57:37 +0000

    Refactor mapTransactions

--- modules/mobile/db/mappers.js
+++ modules/mobile/db/mappers.js
@@ -1,21 +1,31 @@
 import {createLookup} from '@/utils/arrays'
 import {addDays} from 'date-fns'
 
-export const mapTransactions = (dbTxs, dbCats, dbUser) => {
-  if (!dbUser) return []
+export const mapTransactions = (dbTxs, dbCats, dbUser, withFutureTxFiltering = true) => {
   if (dbCats === null || dbCats.length === 0) return []
 
   const catsById = createLookup(dbCats, c => c.id, c => c.toDomain)
-  const user = dbUser.toDomain
-  const maxTxDate = addDays(new Date(), user?.settings?.futureTransactionVisibilityDays || 0)
+  const futureTxFilter = withFutureTxFiltering ? deriveFutureTxFilter(dbUser) : deriveFutureTxFilter(null)
   return dbTxs
       .map(t => ({...t.toDomain, category: catsById[t.categoryId]}))
       .filter(t => {
         return (t?.hidden !== true) &&
             (t?.category && t?.category?.hidden !== true) &&
-            (t?.amount?.currency?.code === user?.settings?.currency?.code) &&
-            (user.settings?.futureTransactionVisibilityDays === null || new Date(t.date) <= maxTxDate)
+            (t?.amount?.currency?.code === dbUser?.settingsCurrencyCode) &&
+            (futureTxFilter.allowAll || new Date(t.date) <= futureTxFilter.maxDate)
       })
+}
+
+const deriveFutureTxFilter = (dbUser) => {
+  if (!dbUser) {
+    return {allowAll: true, maxDate: null}
+  }
+  const user = dbUser.toDomain
+  const maxDate = addDays(new Date(), user?.settings?.futureTransactionVisibilityDays || 0)
+  return {
+    maxDate,
+    allowAll: user.settings?.futureTransactionVisibilityDays === null
+  }
 }
 
 export const mapCategories = (dbCats) => {
