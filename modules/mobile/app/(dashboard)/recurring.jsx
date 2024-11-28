192708cfb5300b8147ab246a8363c7bdb46f742c
kirill5k <immotional@aol.com>
2024-11-28 16:45:14 +0000

    Remove unused code from recurring screen

--- modules/mobile/app/(dashboard)/recurring.jsx
+++ modules/mobile/app/(dashboard)/recurring.jsx
@@ -1,23 +1,10 @@
-import {useEffect, useState} from 'react'
 import {VStack} from '@/components/ui/vstack'
 import {Heading} from '@/components/ui/heading'
 import {Text} from '@/components/ui/text'
 import Classes from '@/constants/classes'
 
-import {generatePeriodicTransactionRecurrenceInstanceId} from '@/db/operations'
-
 
 const Recurring = ({}) => {
-  const [testId, setTestId] = useState(null)
-
-  useEffect(() => {
-    const runTest = async () => {
-      const id = await generatePeriodicTransactionRecurrenceInstanceId("673cb70801452339cd5b4ec1", "2024-10-10")
-      setTestId(id)
-    }
-    runTest()
-  }, []);
-
   return (
       <VStack className={Classes.dashboardLayout}>
         <Heading size="2xl" className="pb-2">
