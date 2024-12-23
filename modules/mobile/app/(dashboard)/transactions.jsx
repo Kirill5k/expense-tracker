44f190abe2a1f4022528babcc2d1f2d04aee165e
kirill5k <immotional@aol.com>
2024-12-23 17:57:21 +0000

    Reduce header on scroll on transactions page

--- modules/mobile/app/(dashboard)/transactions.jsx
+++ modules/mobile/app/(dashboard)/transactions.jsx
@@ -24,6 +24,7 @@ const Transactions = ({state, user, displayedTransactions, categories}) => {
   const {setUndoAlert, setTxToUpdate} = useStore()
   const mode = useColorScheme()
 
+  const [isScrolling, setIsScrolling] = useState(false)
   const [loading, setLoading] = useState(false)
   const [searchQuery, setSearchQuery] = useState('')
   const [filteredCats, setFilteredCats] = useState([])
@@ -51,7 +52,7 @@ const Transactions = ({state, user, displayedTransactions, categories}) => {
   return (
       <VStack className={Classes.dashboardLayout}>
         <HStack className="relative">
-          <Heading size="2xl" className={loading ? 'pb-0' : 'pb-1'}>
+          <Heading size={isScrolling ? 'md' : '2xl'} className={loading ? 'pb-0' : 'pb-1'}>
             Transactions
           </Heading>
           <SearchInput
@@ -81,6 +82,13 @@ const Transactions = ({state, user, displayedTransactions, categories}) => {
             onItemPress={handleItemPress}
             onItemCopy={handleItemCopy}
             onItemDelete={handleItemDelete}
+            onScroll={({nativeEvent}) => {
+              if (nativeEvent.contentOffset.y <= 20 && isScrolling) {
+                setIsScrolling(false)
+              } else if (nativeEvent.contentOffset.y > 20 && !isScrolling) {
+                setIsScrolling(true)
+              }
+            }}
         />
         <FloatingButton
             onPress={() => {
