c805793b78cc5f52d60f4451cef30184e9a5c613
kirill5k <immotional@aol.com>
2025-01-01 09:30:38 +0000

    Increase marging between header and list items

--- modules/mobile/constants/classes.js
+++ modules/mobile/constants/classes.js
@@ -2,7 +2,7 @@ export default {
   dashboardLayout: 'px-4 pt-2 pb-0 md:px-10 md:pt-6 md:pb-0 h-full w-full max-w-[1500px] self-center bg-background-0',
   scrollList: 'max-w-[600px] w-full',
   listItemLayout: 'items-center p-3',
-  listItemHeader: 'mb-2 p-3 rounded-xl bg-background-50 justify-between',
+  listItemHeader: 'mb-4 p-3 rounded-xl bg-background-50 justify-between',
   listItemMainText: 'font-semibold text-primary-900 line-clamp-1 p-0 m-0 text-lg',
   listItemAmount: ' text-lg font-medium ml-auto', // OLD: 'rounded-xl border text-md font-medium p-1 px-2 ml-auto'
   dark: {
