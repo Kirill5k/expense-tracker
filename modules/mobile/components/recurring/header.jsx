7b8f8ccb8164592eaf6290329aac67a9e1169bc8
kirill5k <immotional@aol.com>
2025-01-08 09:55:33 +0000

    Refactor calculateMonthlyTotal

--- modules/mobile/components/recurring/header.jsx
+++ modules/mobile/components/recurring/header.jsx
@@ -4,37 +4,49 @@ import {VStack} from '@/components/ui/vstack'
 import {Text} from '@/components/ui/text'
 import {Heading} from '@/components/ui/heading'
 import {printAmount, isExpense} from '@/utils/transactions'
-import {getDaysInMonth} from 'date-fns'
 import Classes from '@/constants/classes'
 
+const DAY_IN_MS = 1000 * 60 * 60 * 24
+
 const calculateMonthlyTotal = (transactions, currentDate) => {
-  const daysInMonth = getDaysInMonth(currentDate)
+  const currentMonthStart = new Date(currentDate.getFullYear(), currentDate.getMonth(), 1);
+  const currentMonthEnd = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 0);
+
   let totalAmount = 0
 
-  transactions.forEach(transaction => {
+  for (const transaction of transactions) {
     const {recurrence, amount} = transaction
     let occurrences = 0
 
-    switch (recurrence.frequency) {
-      case 'daily':
-        occurrences = Math.floor(daysInMonth / recurrence.interval)
-        break;
-      case 'weekly':
-        occurrences = Math.floor((daysInMonth / 7) / recurrence.interval)
-        break;
-      case 'monthly':
-        occurrences = recurrence.interval === 1 ? 1 : 0
-        break;
-      default:
-        occurrences = 0
-    }
+    const transactionStart = new Date(recurrence.startDate)
+    const transactionEnd = recurrence.endDate ? new Date(recurrence.endDate) : null
 
-    if (isExpense(transaction)) {
-      totalAmount -= occurrences * amount.value
-    } else {
-      totalAmount += occurrences * amount.value
+    if ((transactionStart <= currentMonthEnd) && (!transactionEnd || transactionEnd >= currentMonthStart)) {
+      switch (recurrence.frequency) {
+        case 'daily':
+          const dailyStart = Math.max(currentMonthStart, transactionStart)
+          const dailyEnd = Math.min(currentMonthEnd, transactionEnd || currentMonthEnd)
+          occurrences = Math.floor((dailyEnd - dailyStart) / (recurrence.interval * DAY_IN_MS))
+          break;
+        case 'weekly':
+          const weeklyStart = Math.max(currentMonthStart, transactionStart)
+          const weeklyEnd = Math.min(currentMonthEnd, transactionEnd || currentMonthEnd)
+          occurrences = Math.floor((weeklyEnd - weeklyStart) / (recurrence.interval * 7 * DAY_IN_MS))
+          break;
+        case 'monthly':
+          occurrences = recurrence.interval === 1 ? 1 : 0
+          break;
+        default:
+          occurrences = 0
+      }
+
+      if (isExpense(transaction)) {
+        totalAmount -= occurrences * amount.value
+      } else {
+        totalAmount += occurrences * amount.value
+      }
     }
-  })
+  }
 
   return totalAmount
 }
@@ -58,7 +70,7 @@ const RecurringTransactionHeader = ({items}) => {
   return (
       <HStack className={Classes.listItemHeader}>
         <VStack className="w-2/4">
-          <Text>Monthly Total</Text>
+          <Text>This Month's Total</Text>
           <Heading size="xl">{printAmount(monthTotal, items[0]?.amount?.currency)}</Heading>
         </VStack>
         <Divider orientation="vertical"/>
