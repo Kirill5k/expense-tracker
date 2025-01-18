02549d187cd8e0e5f99712990431e990df3a5a15
kirill5k <immotional@aol.com>
2025-01-18 09:04:22 +0000

    Tidy up GluestackUIProvider

--- modules/mobile/components/ui/gluestack-ui-provider/index.jsx
+++ modules/mobile/components/ui/gluestack-ui-provider/index.jsx
@@ -1,23 +1,19 @@
 import React from 'react';
 import { config } from './config';
-import { ColorSchemeName, useColorScheme, View, ViewProps } from 'react-native';
+import { useColorScheme, View } from 'react-native';
 import { OverlayProvider } from '@gluestack-ui/overlay';
 import { ToastProvider } from '@gluestack-ui/toast';
 import { colorScheme as colorSchemeNW } from 'nativewind';
 
 const getColorSchemeName = (colorScheme, mode) => {
   if (mode === 'system') {
-    return colorScheme ?? 'light';
+    return colorScheme || 'light';
   }
   return mode;
 };
 
-export function GluestackUIProvider({
-  mode = 'light',
-  ...props
-}) {
+export function GluestackUIProvider({mode = 'light', ...props}) {
   const colorScheme = useColorScheme();
-
   const colorSchemeName = getColorSchemeName(colorScheme, mode);
 
   colorSchemeNW.set(mode);
