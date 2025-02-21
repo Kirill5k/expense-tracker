83d3ad244079e2d29b656a839cba34d9b7e0a653
kirill5k <immotional@aol.com>
2025-02-21 09:23:13 +0000

    Fix padding

--- modules/mobile/app/(dashboard)/analytics.jsx
+++ modules/mobile/app/(dashboard)/analytics.jsx
@@ -59,7 +59,7 @@ const Analytics = ({state, user, displayedTransactions, categories, previousDisp
               onChartPress={setSelectedTransactions}
           />
           <DatePeriodSelect
-              className="mb-2 mt-4"
+              className="mb-2 mt-3"
               disabled={loading}
               mode={mode}
               value={state.displayDate}
