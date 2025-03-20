63c01e92616a05a46561407eda9639b4b557b800
kirill5k <immotional@aol.com>
2025-03-20 15:29:09 +0000

    Refactor tx filter

--- modules/mobile/components/transaction/filter.jsx
+++ modules/mobile/components/transaction/filter.jsx
@@ -1,5 +1,7 @@
 import {useState} from 'react'
 import {VStack} from '@/components/ui/vstack'
+import {HStack} from '@/components/ui/hstack'
+import {Text} from '@/components/ui/text'
 import {Heading} from '@/components/ui/heading'
 import {Slider, SliderTrack, SliderFilledTrack, SliderThumb} from '@/components/ui/slider'
 import {MaterialIcon, CheckIcon} from '@/components/ui/icon'
@@ -30,55 +32,20 @@ const SliderMax = 500
 const MinAmount = 1
 const MaxAmount = 50000
 
-function amountToSliderValue(amount) {
-  const logMin = Math.log10(MinAmount)
-  const logMax = Math.log10(MaxAmount)
-  const logAmount = Math.log10(amount)
-
-  // Map logarithmic amount to slider value
-  return ((logAmount - logMin) / (logMax - logMin)) * SliderMax;
-}
-
-function sliderValueToAmount(sliderValue) {
-  const logMin = Math.log10(MinAmount)
-  const logMax = Math.log10(MaxAmount)
-
-  // Map slider value to logarithmic amount
-  const logAmount = logMin + (sliderValue / SliderMax) * (logMax - logMin);
-
-  // Convert logarithmic amount back to linear scale
-  return Math.round(Math.pow(10, logAmount))
-}
-
 const TransactionFilter = ({mode, className, categories, value, onChange}) => {
   const [show, setShow] = useState(false)
   const handleClose = () => setShow(false)
 
   const handleCategoryPress = (id) => {
-    if (value.categories.includes(id)) {
-      onChange({
-        ...value,
-        categories: value.categories.filter(i => i !== id)
-      })
-    } else {
-      onChange({
-        ...value,
-        categories: [...value.categories, id]
-      })
-    }
+    const updatedCats = value.categories.includes(id)
+        ? value.categories.filter(i => i !== id)
+        : [...value.categories, id]
+    onChange({...value, categories: updatedCats})
   }
 
   const hasSelectedFilters = value?.categories?.length
       || value.minAmount > MinAmount
       || value.maxAmount < MaxAmount
-
-  const handleMinAmountChange = v => {
-    onChange({...value, minAmount: sliderValueToAmount(v)})
-  }
-
-  const handleMaxAmountChange = v => {
-    onChange({...value, maxAmount: sliderValueToAmount(v)})
-  }
 
   return (
       <>
@@ -103,37 +70,19 @@ const TransactionFilter = ({mode, className, categories, value, onChange}) => {
               <ActionsheetDragIndicator/>
             </ActionsheetDragIndicatorWrapper>
             <VStack className="w-full px-3" space="lg">
-              <Heading>Min Amount: {value.minAmount || MinAmount}</Heading>
-              <Slider
-                  defaultValue={MinAmount}
-                  step={SliderStep}
-                  minValue={1}
-                  maxValue={SliderMax}
-                  sliderTrackHeight={16}
-                  value={amountToSliderValue(value.minAmount || MinAmount)}
-                  onChange={handleMinAmountChange}
-              >
-                <SliderTrack>
-                  <SliderFilledTrack/>
-                </SliderTrack>
-                <SliderThumb/>
-              </Slider>
-              <Heading>Max Amount: {value.maxAmount || MaxAmount}</Heading>
-              <Slider
+              <AmountSlider
+                heading="Min Amount"
+                defaultValue={MinAmount}
+                value={value.minAmount}
+                onChange={(v) => onChange({...value, minAmount: v})}
+              />
+              <AmountSlider
+                  heading="Max Amount"
                   defaultValue={MaxAmount}
-                  step={SliderStep}
-                  minValue={1}
-                  maxValue={SliderMax}
-                  sliderTrackHeight={16}
-                  value={amountToSliderValue(value.maxAmount || MaxAmount)}
-                  onChange={handleMaxAmountChange}
-              >
-                <SliderTrack>
-                  <SliderFilledTrack/>
-                </SliderTrack>
-                <SliderThumb/>
-              </Slider>
-              <Heading>Categories:</Heading>
+                  value={value.maxAmount}
+                  onChange={(v) => onChange({...value, maxAmount: v})}
+              />
+              <Heading>Categories</Heading>
             </VStack>
             <ScrollView className="w-full h-52">
               <CheckboxGroup
@@ -163,6 +112,55 @@ const TransactionFilter = ({mode, className, categories, value, onChange}) => {
             </ScrollView>
           </ActionsheetContent>
         </Actionsheet>
+      </>
+  )
+}
+
+const AmountSlider = ({value, onChange, heading, defaultValue}) => {
+  const amountToSliderValue = (amount) => {
+    const logMin = Math.log10(MinAmount)
+    const logMax = Math.log10(MaxAmount)
+    const logAmount = Math.log10(amount)
+
+    // Map logarithmic amount to slider value
+    return ((logAmount - logMin) / (logMax - logMin)) * SliderMax;
+  }
+
+  const sliderValueToAmount = (sliderValue) => {
+    const logMin = Math.log10(MinAmount)
+    const logMax = Math.log10(MaxAmount)
+
+    // Map slider value to logarithmic amount
+    const logAmount = logMin + (sliderValue / SliderMax) * (logMax - logMin);
+
+    // Convert logarithmic amount back to linear scale
+    return Math.round(Math.pow(10, logAmount))
+  }
+
+  const handleChange = (v) => {
+    onChange(sliderValueToAmount(v))
+  }
+
+  return (
+      <>
+        <HStack className="justify-between">
+          <Heading>{heading}</Heading>
+          <Text size="md">{value || defaultValue}</Text>
+        </HStack>
+        <Slider
+            defaultValue={amountToSliderValue(defaultValue)}
+            step={SliderStep}
+            minValue={1}
+            maxValue={SliderMax}
+            sliderTrackHeight={16}
+            value={amountToSliderValue(value || defaultValue)}
+            onChange={handleChange}
+        >
+          <SliderTrack>
+            <SliderFilledTrack/>
+          </SliderTrack>
+          <SliderThumb/>
+        </Slider>
       </>
   )
 }
