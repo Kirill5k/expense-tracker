78a3df5942acb14a995086b40c54f3193926888f
kirill5k <immotional@aol.com>
2025-04-09 11:19:46 +0100

    refactor tx list

--- modules/mobile/components/transaction/list.jsx
+++ modules/mobile/components/transaction/list.jsx
@@ -16,80 +16,79 @@ import {FlashList} from '@shopify/flash-list'
 import TransactionHeader from './header'
 import {mergeClasses} from '@/utils/css'
 
-const TransactionGroup = React.memo(({mode, disabled, items, onItemPress, onItemCopy, onItemDelete}) => {
+// height 38.5
+const TransactionGroupHeader = ({date, total}) => {
   return (
-      <VStack className="rounded-xl bg-background-50 p-1" space="sm">
-        {items.map(tx => (
-            <ListItemPressable
-                disabled={disabled}
-                key={tx.id}
-                onPress={() => onItemPress(tx)}
-                onCopy={() => onItemCopy(tx)}
-                onDelete={() => onItemDelete(tx)}
-            >
-              <HStack className={Classes.listItemLayout + ' '}>
-                <ListItemIcon
-                    icon={tx.category.icon}
-                    color={tx.category.color}
-                />
-                <VStack className="justify-center gap-1">
-                  <HStack space="xs" className="items-center">
-                    <Text className={Classes.listItemMainText}>
-                      {tx.note || tx.category.name}
-                    </Text>
-                    {tx.isRecurring && (
-                        <Badge
-                            className="bg-transparent p-0 pb-0.5"
-                            variant="solid"
-                        >
-                          <BadgeIcon
-                              className="text-white"
-                              as={MaterialIcon}
-                              code="repeat-variant"
-                              dsize={20}
-                              dcolor={Colors[mode].tabIconDefault}
-                          />
-                        </Badge>
-                    )}
-                  </HStack>
-                  <TagList items={tx.tags} className="max-w-64"/>
-                </VStack>
-                <Text
-                    className={mergeClasses(
-                        Classes.listItemAmount,
-                        isExpense(tx) ? 'text-red-500' : 'text-green-500'
-                    )}>
-                  {formatAmount(tx)}
-                </Text>
-              </HStack>
-            </ListItemPressable>
-        ))}
-      </VStack>
+      <HStack className="bg-background-0 items-center justify-between py-2">
+        <Heading size="lg">{date}</Heading>
+        <Text className="text-lg">{total}</Text>
+      </HStack>
   )
-})
+}
 
-const TransactionListItem = React.memo(({mode, disabled, item, onItemPress, onItemCopy, onItemDelete}) => {
+// height 63
+const TransactionListItem = ({item, mode, disabled, onPress, onCopy, onDelete}) => {
   return (
-      <VStack className="mb-5">
-        <HStack className="items-center justify-between py-2">
-          <Heading size="lg">{formatDate(item)}</Heading>
-          <Text className="text-lg">{printAmount(calcTotal(item.txGroup), item.txGroup[0].amount.currency)}</Text>
+      <ListItemPressable
+          disabled={disabled}
+          onPress={() => onPress(item)}
+          onCopy={() => onCopy(item)}
+          onDelete={() => onDelete(item)}
+      >
+        <HStack className={Classes.listItemLayout + ' '}>
+          <ListItemIcon
+              icon={item.category.icon}
+              color={item.category.color}
+          />
+          <VStack className="justify-center gap-1">
+            <HStack space="xs" className="items-center">
+              <Text className={Classes.listItemMainText}>
+                {item.note || item.category.name}
+              </Text>
+              {item.isRecurring && (
+                  <Badge
+                      className="bg-transparent p-0 pb-0.5"
+                      variant="solid"
+                  >
+                    <BadgeIcon
+                        className="text-white"
+                        as={MaterialIcon}
+                        code="repeat-variant"
+                        dsize={20}
+                        dcolor={Colors[mode].tabIconDefault}
+                    />
+                  </Badge>
+              )}
+            </HStack>
+            <TagList items={item.tags} className="max-w-64"/>
+          </VStack>
+          <Text
+              className={mergeClasses(
+                  Classes.listItemAmount,
+                  isExpense(item) ? 'text-red-500' : 'text-green-500'
+              )}>
+            {formatAmount(item)}
+          </Text>
         </HStack>
-        <TransactionGroup
-            mode={mode}
-            disabled={disabled}
-            items={item.txGroup}
-            onItemPress={onItemPress}
-            onItemCopy={onItemCopy}
-            onItemDelete={onItemDelete}
-        />
-      </VStack>
+      </ListItemPressable>
   )
-})
+}
 
 const TransactionList = ({mode, disabled, items, onItemPress, onItemCopy, onItemDelete, onScroll}) => {
   const groupedItems = Object.entries(groupBy(items, i => i.date))
-  const data = groupedItems.map(([date, txGroup]) => ({date, txGroup}))
+  const data = groupedItems.flatMap(([date, txGroup]) => {
+    const header = {
+      isHeader: true,
+      date: formatDate({date}),
+      total: printAmount(calcTotal(txGroup), txGroup[0].amount.currency)
+    }
+    const items = txGroup.map((item, i) => ({...item, isFirst: i === 0, isLast: i === txGroup.length - 1}))
+    return [header, ...items]
+  })
+
+  const stickyHeaderIndices = data
+      .map((item, i) => item.isHeader ? i : null)
+      .filter((item) => item !== null)
 
   const flatListRef = useRef(null)
   const firstItem = items.length > 0 ? items[0].id : null
@@ -108,22 +107,35 @@ const TransactionList = ({mode, disabled, items, onItemPress, onItemCopy, onItem
           showsVerticalScrollIndicator={false}
           className={Classes.scrollList}
           initialNumToRender={10}
-          estimatedItemSize={110}
+          estimatedItemSize={63}
           data={data}
-          keyExtractor={(item) => item.date}
+          stickyHeaderIndices={stickyHeaderIndices}
+          keyExtractor={(item) => item.id || item.date}
           renderItem={({item}) => (
-              <TransactionListItem
-                  mode={mode}
-                  item={item}
-                  disabled={disabled}
-                  onItemPress={onItemPress}
-                  onItemCopy={onItemCopy}
-                  onItemDelete={onItemDelete}
-              />
+              item.isHeader ? (
+                  <TransactionGroupHeader date={item.date} total={item.total}/>
+              ) : (
+                  <Box className={mergeClasses(
+                      'px-1 py-0.5 bg-background-50',
+                      item.isFirst && 'rounded-t-xl pt-1',
+                      item.isLast && 'rounded-b-xl pb-1 mb-5'
+                  )}>
+                    <TransactionListItem
+                        key={item.id}
+                        mode={mode}
+                        disabled={disabled}
+                        item={item}
+                        onPress={onItemPress}
+                        onCopy={onItemCopy}
+                        onDelete={onItemDelete}
+                    />
+                  </Box>
+              )
           )}
           ListHeaderComponent={<TransactionHeader items={items}/>}
           ListEmptyComponent={<Text className="py-10 text-center">No transactions for this period</Text>}
           ListFooterComponent={<Box className="py-3"></Box>}
+          getItemType={(item) => typeof item.isHeader ? 'sectionHeader' : 'row'}
       />
   )
 }
