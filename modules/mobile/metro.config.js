4e5532b0227a290d6778646e44dfeb0684833a08
kirill5k <immotional@aol.com>
2025-08-18 11:58:41 +0100

    Bump mobile dependencies

--- modules/mobile/metro.config.js
+++ modules/mobile/metro.config.js
@@ -2,5 +2,10 @@ const { getDefaultConfig } = require('expo/metro-config');
 const { withNativeWind } = require('nativewind/metro');
   
 const config = getDefaultConfig(__dirname);
-  
+
+config.resolver.alias = {
+  ...config.resolver.alias,
+  buffer: require.resolve('buffer'),
+};
+
 module.exports = withNativeWind(config, { input: './global.css' });
