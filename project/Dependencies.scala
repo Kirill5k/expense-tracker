88d8da358a5caff213af98079b99ab2cb69ab838
kirill5k <immotional@aol.com>
2026-07-19 11:27:05 +0100

    Bump dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -10,9 +10,9 @@ object Dependencies {
     val squants      = "1.8.3"
     val bcrypt       = "4.3.0"
     val refined      = "0.11.4"
-    val logback      = "1.5.37"
+    val logback      = "1.5.38"
     val log4cats     = "2.8.0"
-    val tapir        = "1.13.25"
+    val tapir        = "1.13.28"
     val jwt          = "11.0.4"
     val apispec      = "0.11.3"
   }
