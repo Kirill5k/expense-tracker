e3639846329e3178660b664c93f1f6fb99a9879c
kirill5k <immotional@aol.com>
2026-08-02 17:04:01 +0100

    Bump sbt version

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -10,9 +10,9 @@ object Dependencies {
     val squants      = "1.8.3"
     val bcrypt       = "4.3.0"
     val refined      = "0.11.4"
-    val logback      = "1.6.0"
+    val logback      = "1.6.1"
     val log4cats     = "2.8.0"
-    val tapir        = "1.13.29"
+    val tapir        = "1.13.30"
     val jwt          = "11.0.4"
     val apispec      = "0.11.3"
   }
